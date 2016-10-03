983bd31ebf48a11ed768f7e431e0b6b1e6afaff8
atscomposer <atscomposer@gmail.com>
2016-10-03 16:17:27 -0400

    Initial Upload

---
+++ FindADAttributeLengths.ps1
@@ -0,0 +1,4 @@
+﻿$schema =[DirectoryServices.ActiveDirectory.ActiveDirectorySchema]::GetCurrentSchema()
+## I'm looking for user attributes and the property I'm looking for is an optional attribute
+## The rangeUpper attribute is what tells us the max length of what can go into that particular attribute
+$schema.FindClass('user').optionalproperties | select name,rangeupper | where {$_.rangeupper -ne $null} | fl
